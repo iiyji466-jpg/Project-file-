beaba5438fe433594456e3f3d07185d0bf5b37b5
iiyji466-jpg <iiyji466@gmail.com>
2026-04-23 21:06:50 +0300

    Create _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="ar" dir="rtl">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="theme-color" content="#0f0f1a" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body><Main /><NextScript /></body>
+    </Html>
+  )
+}
